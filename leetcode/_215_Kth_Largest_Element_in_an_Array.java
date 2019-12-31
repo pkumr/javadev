634f2e8a218af27868abb4c6385882e88c3c0050
Parveen Kumar <parveen.kumar6@gmail.com>
2019-12-30 18:54:43 -0800

    adding method to solve problem using heap data structure

--- leetcode/_215_Kth_Largest_Element_in_an_Array.java
+++ leetcode/_215_Kth_Largest_Element_in_an_Array.java
@@ -1,7 +1,20 @@
 import java.util.*;
 public class _215_Kth_Largest_Element_in_an_Array{
+  //Approach#1 : Brute Force Approach
   public int findKthLargest(int[] nums, int k){
     Arrays.sort(nums);
     return nums[nums.length - k];
+  }
+
+  //Approach#2 : Using Heap Datastructure
+  public int findKthLargestMinHeap(int[] nums, int k){
+    PriorityQueue<Integer> minheap = new PriorityQueue<>();
+    for(int num : nums){
+      minheap.add(num);
+      if(minheap.size() > k){
+        minheap.remove();
+      }
+    }
+    return minheap.poll();
   }
 }
