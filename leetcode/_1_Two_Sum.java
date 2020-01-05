69eeb34a3c9333fcaba346ade39c4e4ff80baa6e
Parveen Kumar <parveen.kumar6@gmail.com>
2020-01-04 19:21:08 -0800

    added using hash table

--- leetcode/_1_Two_Sum.java
+++ leetcode/_1_Two_Sum.java
@@ -4,10 +4,13 @@
 *				such that they add up to a specific target.
 *
 ***************************************************************************/
-public class _1_Two_Sum{
+import java.util.*;
 
+public class _1_Two_Sum{
+	//****************Brute Force Approach*******************
+	//Time Compexity - O(N-Square)
 	public int[] twoSum_bruteforce(int[] nums, int target) {
-        //****************Brute Force Approach*******************
+
         //Consider nums = {1, 2, 3, 4, 5} and target = 7
         //for loop with variable from 1 to 5
         for(int i = 0; i < nums.length; i++){
@@ -22,4 +25,27 @@ public class _1_Two_Sum{
             }
         }
         throw new IllegalArgumentException("No Two Sum Solution");
+    }
+
+    //*******************Using Hash Table***************************
+    public int[] twoSum_hashtable(int[] nums, int target) {
+        /*************Using Hash Table - Space - O(N) and Time - O(N)***/
+        Map<Integer, Integer> htab = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            //iterate through array, add values and 
+            //positions of values in hash table
+            htab.put(nums[i], i);
+        }
+        for(int i = 0; i < nums.length; i++){
+            int diff = target - nums[i];
+            //take the difference that comes from subtracting value at ith position
+            //Go to hash table, check if we have key in hashtable --htab.containsKey(diff) -- true
+            //and also check if diff value is not at ith position
+            if(htab.containsKey(diff) && htab.get(diff) != i){
+                return new int[] {i, htab.get(diff)};
+            }
+        }
+        throw new IllegalArgumentException("No Two Solutions");
+    }
+
 }
