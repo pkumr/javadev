afa01ad57b98b448c26f1553e84f0b88e1c85248
Parveen Kumar <parveen.kumar6@gmail.com>
2020-01-09 23:41:48 -0800

    gas station problem

---
+++ leetcode/_134_GasStation.java
@@ -0,0 +1,17 @@
+public class _134_GasStation {
+	public int canCompleteCircuit(int[] gas, int[] cost){
+		int n = gas.length;
+		int canbeCompleted = 0;
+		int visited = 0;
+		int startingIndex = 0;
+		for(int i = 0; i < n; i++){
+			canbeCompleted += gas[i] - cost[i];
+			visited += gas[i] -  cost[i];
+			if(visited < 0){
+				startingIndex = i + 1;
+				visited = 0;
+			}
+		}
+		return canbeCompleted >= 0 startingIndex : -1;
+	}
+}
