e7ceb097e87a4d2073ea314c743ba210336a244e
Parveen Kumar <parveen.kumar6@gmail.com>
2020-01-12 22:11:36 -0800

    all algorithms

---
+++ algorithms/greedy/SchedulingProblem.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pk.algorithms.greedy;
+
+/**
+ *
+ * @author parveenkumar
+ */
+public class SchedulingProblem {
+    
+}
---
+++ algorithms/slidingwindow/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pk.algorithms.slidingwindow;
+
+/**
+ *
+ * @author parveenkumar
+ * Reference - https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+    
+}
