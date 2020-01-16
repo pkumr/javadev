4ee890faf9f240180e2eaba979c3b8b7abc98cc1
Parveen Kumar <parveen.kumar6@gmail.com>
2020-01-15 23:03:20 -0800

    Subset Problem with duplicates - backtracking

---
+++ algorithms/backtracking/SubsetsII.java
@@ -0,0 +1,27 @@
+package com.pk.algorithms.backtracking;
+
+/**
+ *
+ * @author parveenkumar
+ */
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+public class SubsetsII {
+  public List<List<Integer>> subsetWithDup(int[] nums){
+      List<List<Integer>> list = new ArrayList<>();
+      Arrays.sort(nums);
+      backtrack(list, new ArrayList<>(), nums, 0);
+      return list;
+  }
+  private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int start){
+      list.add(new ArrayList<>(tempList));
+      for(int i = start; i < nums.length; i++){
+          if(i > start && nums[i] == nums[i - 1]) continue; //skip duplicates
+          tempList.add(nums[i]);
+          backtrack(list, tempList, nums, i + 1);
+          tempList.remove(tempList.size() - 1);
+      }
+  }
+}
