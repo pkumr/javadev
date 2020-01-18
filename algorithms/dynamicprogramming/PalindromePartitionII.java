59fbac225d38f53cba6e48162ae9cb3073e251f8
Parveen Kumar <parveen.kumar6@gmail.com>
2020-01-17 20:34:11 -0800

    Palindrome Parition - DP, first soln - backtracking

---
+++ algorithms/dynamicprogramming/PalindromePartitionII.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pk.algorithms.dynamicprogramming;
+
+/**
+ *
+ * @author parveenkumar
+ */
+import java.util.stream.IntStream;
+
+public class PalindromePartitionII {
+    public int minCut(String s) {
+        // validate input
+        if (s == null || s.length() <= 1) {
+            return 0;
+        }
+        // dp
+        int N = s.length();
+        int[] dp = IntStream.range(0, N).toArray(); // initial value: dp[i] = i
+
+        for (int mid = 1; mid <  N; mid++) { 
+            // iterate through all chars as mid point of palindrome
+            // CASE 1. odd len: center is at index mid, expand on both sides
+            for (int start = mid, end = mid; start >= 0 && end < N && s.charAt(start) == s.charAt(end); start--, end++) {
+                int newCutAtEnd = (start == 0) ? 0 : dp[start - 1] + 1;
+                dp[end] = Math.min(dp[end], newCutAtEnd);
+            }
+            // CASE 2: even len: center is between [mid-1,mid], expand on both sides
+            for (int start = mid - 1, end = mid; start >= 0 && end < N && s.charAt(start) == s.charAt(end); start--, end++) {
+                int newCutAtEnd = (start == 0) ? 0 : dp[start - 1] + 1;
+                dp[end] = Math.min(dp[end], newCutAtEnd);
+            }
+        }
+        return dp[N - 1];
+    }
+}
