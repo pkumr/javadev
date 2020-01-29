a01742a6dc9f4951b83728fc6ad82a00c1f50b23
Parveen Kumar <parveen.kumar6@gmail.com>
2020-01-28 22:52:21 -0800

    Level Order Traversal of BT

--- algorithms/trees/BinaryTreeLevelOrderTraversal.java
+++ algorithms/trees/BinaryTreeLevelOrderTraversal.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *Level Order Traversal of BT
  */
 package com.pk.algorithms.trees;
 
@@ -9,6 +7,47 @@ package com.pk.algorithms.trees;
  *
  * @author parveenkumar
  */
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
+
 public class BinaryTreeLevelOrderTraversal {
+    //Definition of Binary Tree Node
+    public static class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x){
+            this.val = x;
+        }
+    }
     
+    public List<List<Integer>> levelOrderTraversal(TreeNode root){
+        //Store output list in result variable
+        List<List<Integer>> result = new ArrayList<>();
+         if(root == null)
+            return result;
+        //Using Breath First Traversal
+        //Declare a queue
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            //Declare another list of integers to store current level
+            List<Integer> currentLevel = new LinkedList<>();
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                TreeNode currentNode = queue.remove();
+                currentLevel.add(currentNode.val);
+                if(currentNode.left != null){
+                    queue.add(currentNode.left);
+                }
+                if(currentNode.right != null){
+                    queue.add(currentNode.right);
+                }
+            }
+            result.add(currentLevel);
+        }
+        return result;
+    }
 }
