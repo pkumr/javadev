542a36d0c28f2ce5242538528cde3d8763c0b4cb
Parveen Kumar <parveen.kumar6@gmail.com>
2020-01-29 23:31:17 -0800

    unique triplets which gives sum of zero

---
+++ leetcode/_15_3Sum.java
@@ -0,0 +1,39 @@
+import java.util.List;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class _15_3Sum {
+    /*
+    *
+    * Given an array nums of n integers, are there elements a, b, c 
+    * in nums such that a + b + c = 0? Find all unique triplets in 
+    * the array which gives the sum of zero.
+    *
+    */
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new LinkedList<>();
+        Arrays.sort(nums);
+        
+        for(int i = 0; i < nums.length - 2; i++){
+            if(i == 0 || (i > 0 && nums[i] != nums[i - 1])){
+                int low = i + 1;
+                int high = nums.length - 1;
+                int sum = 0 - nums[i];
+                while(low < high){
+                    if(nums[low] + nums[high] == sum){
+                        result.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                        while(low < high && nums[low] == nums[low + 1]) low++;
+                        while(low < high && nums[high] == nums[high - 1]) high--;
+                        low++;
+                        high--;
+                    }else if (nums[low] + nums[high] < sum){
+                        low++;
+                    }else{
+                        high--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
