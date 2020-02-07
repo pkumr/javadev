b6524e665d9ee02ed9162991e2ef00de2ad584e3
Parveen Kumar <parveen.kumar6@gmail.com>
2020-02-06 23:12:39 -0800

    LRU Cache

--- algorithms/design/LRUCache.java
+++ algorithms/design/LRUCache.java
@@ -1,14 +1,100 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 146
  */
 package com.pk.algorithms.design;
-
+import java.util.Map;
+import java.util.HashMap;
 /**
  *
  * @author parveenkumar
  */
 public class LRUCache {
+    //Doubly LinkedList Node
+    class DLinkedNode{
+            int key;
+            int value;
+            DLinkedNode prev;
+            DLinkedNode next;
+    }
+    private void addNode(DLinkedNode node){
+            //Always add the new node right after head.
+            node.prev = head;
+            node.next = head.next;
+            
+            head.next.prev = node;
+            head.next = node;
+    }
+    private void removeNode(DLinkedNode node){
+            //Remove an existing node from the linked list.
+            DLinkedNode prev = node.prev;
+            DLinkedNode next = node.next;
+            
+            prev.next = next;
+            next.prev = prev;
+    }
+    private void moveToHead(DLinkedNode node){
+            //Move certain node in between to the head
+            removeNode(node);
+            addNode(node);
+    }
+    private DLinkedNode popTail(){
+            //Pop the current tail.
+            DLinkedNode result = tail.prev;
+            removeNode(result);
+            return result;
+    }
     
+    private Map<Integer, DLinkedNode> cache = new HashMap<>();
+    private int size;
+    private int capacity;
+    private DLinkedNode head, tail;
+    
+    public LRUCache(int capacity) {
+        this.size = 0;
+        this.capacity = capacity;
+        
+        head = new DLinkedNode();
+        //head.prev = null;
+        
+        tail = new DLinkedNode();
+        //tail.next = null;
+        
+        head.next = tail;
+        tail.prev = head;
+    }
+    
+    public int get(int key) {
+        DLinkedNode node = cache.get(key);
+        if(node == null) return -1;
+        
+        //move the accessed node to the head;
+        moveToHead(node);
+        return node.value;
+        
+    }
+    
+    public void put(int key, int value) {
+        DLinkedNode node = cache.get(key);
+        
+        if(node == null){
+            DLinkedNode newNode = new DLinkedNode();
+            newNode.key = key;
+            newNode.value = value;
+            
+            cache.put(key, newNode);
+            addNode(newNode);
+            ++size;
+            
+            if(size > capacity){
+                //pop the tail
+                DLinkedNode tail = popTail();
+                cache.remove(tail.key);
+                --size;
+            }
+        }else{
+            //Update the value
+            node.value = value;
+            moveToHead(node);
+        }
+    }
 }
