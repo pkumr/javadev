4dd83d1cfe6cbecef446029310ce621d814b769b
Parveen Kumar <parveen.kumar6@gmail.com>
2020-02-06 23:15:30 -0800

    Excel Formula

---
+++ algorithms/design/ExcelSumFormula.java
@@ -0,0 +1,27 @@
+/*
+ * 631
+ */
+package com.pk.algorithms.design;
+
+/**
+ *
+ * @author parveenkumar
+ */
+public class ExcelSumFormula {
+    public ExcelSumFormula(int H, char W) {
+        
+    }
+    
+    public void set(int r, char c, int v) {
+        
+    }
+    
+    public int get(int r, char c) {
+        return -1;
+        
+    }
+    
+    public int sum(int r, char c, String[] strs) {
+        return -1;
+    }
+}
