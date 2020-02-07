1a81f975c6a099ac2d37058fa3b3db0db55ce244
Parveen Kumar <parveen.kumar6@gmail.com>
2020-02-06 23:16:04 -0800

    Hash Map

---
+++ algorithms/design/HashMap.java
@@ -0,0 +1,31 @@
+/*
+ * 706
+ */
+package com.pk.algorithms.design;
+
+/**
+ *
+ * @author parveenkumar
+ */
+public class HashMap {
+    /** Initialize your data structure here. */
+    public HashMap() {
+        
+    }
+    
+    /** value will always be non-negative. */
+    public void put(int key, int value) {
+        
+    }
+    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    public int get(int key) {
+        return -1;
+    }
+    
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    public void remove(int key) {
+        
+    }
+    
+}
