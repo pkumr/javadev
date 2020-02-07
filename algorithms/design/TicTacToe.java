16ab150179b83bd80554dd2aed6fae0577d5f64c
Parveen Kumar <parveen.kumar6@gmail.com>
2020-02-06 23:14:47 -0800

    TicTacToe

---
+++ algorithms/design/TicTacToe.java
@@ -0,0 +1,27 @@
+/*
+ * 348
+ */
+package com.pk.algorithms.design;
+
+/**
+ *
+ * @author parveenkumar
+ */
+public class TicTacToe {
+    /** Initialize your data structure here. */
+    public TicTacToe(int n) {
+        
+    }
+    
+    /** Player {player} makes a move at ({row}, {col}).
+        @param row The row of the board.
+        @param col The column of the board.
+        @param player The player, can be either 1 or 2.
+        @return The current winning condition, can be either:
+                0: No one wins.
+                1: Player 1 wins.
+                2: Player 2 wins. */
+    public int move(int row, int col, int player) {
+        return -1;
+    }
+}
