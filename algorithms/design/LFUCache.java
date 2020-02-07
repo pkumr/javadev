c7ea0a2b12582832f6a6db5041dd07e05595d8f1
Parveen Kumar <parveen.kumar6@gmail.com>
2020-02-06 23:10:34 -0800

    LFU Cache

--- algorithms/design/LFUCache.java
+++ algorithms/design/LFUCache.java
@@ -1,14 +1,57 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 460
  */
 package com.pk.algorithms.design;
-
+import java.util.HashMap;
+import java.util.LinkedHashSet;
 /**
  *
  * @author parveenkumar
  */
 public class LFUCache {
+    HashMap<Integer, Integer> vals;
+    HashMap<Integer, Integer> counts;
+    HashMap<Integer, LinkedHashSet<Integer>> lists;
+    int cap;
+    int min = -1;
+    public LFUCache(int capacity) {
+        cap = capacity;
+        vals = new HashMap<>();
+        counts = new HashMap<>();
+        lists = new HashMap<>();
+        lists.put(1, new LinkedHashSet<>());
+    }
     
+    public int get(int key) {
+        if(!vals.containsKey(key))
+            return -1;
+        int count = counts.get(key);
+        counts.put(key, count+1);
+        lists.get(count).remove(key);
+        if(count==min && lists.get(count).size()==0)
+            min++;
+        if(!lists.containsKey(count+1))
+            lists.put(count+1, new LinkedHashSet<>());
+        lists.get(count+1).add(key);
+        return vals.get(key);
+    }
+    
+    public void put(int key, int value) {
+        if(cap<=0)
+            return;
+        if(vals.containsKey(key)) {
+            vals.put(key, value);
+            get(key);
+            return;
+        } 
+        if(vals.size() >= cap) {
+            int evit = lists.get(min).iterator().next();
+            lists.get(min).remove(evit);
+            vals.remove(evit);
+        }
+        vals.put(key, value);
+        counts.put(key, 1);
+        min = 1;
+        lists.get(1).add(key);
+    }
 }
