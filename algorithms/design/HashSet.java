1cd6c983429684bef713bd2849d3ebc98b253fa4
Parveen Kumar <parveen.kumar6@gmail.com>
2020-02-06 23:16:38 -0800

    Hash Set

---
+++ algorithms/design/HashSet.java
@@ -0,0 +1,28 @@
+/*
+ * 705
+ */
+package com.pk.algorithms.design;
+
+/**
+ *
+ * @author parveenkumar
+ */
+public class HashSet {
+    /** Initialize your data structure here. */
+    public HashSet() {
+        
+    }
+    
+    public void add(int key) {
+        
+    }
+    
+    public void remove(int key) {
+        
+    }
+    
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        return false;
+    }
+}
