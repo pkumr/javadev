ce8fc13de33e6a284bf0186138f074b438673f3e
Parveen Kumar <parveen.kumar6@gmail.com>
2020-02-08 22:58:07 -0800

    CoinChange

--- algorithms/dynamicprogramming/CoinChangeProblem.java
+++ algorithms/dynamicprogramming/CoinChangeProblem.java
@@ -1,7 +1,23 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Reference - Leetcode
+ * 322. Coin Change
+ * 
+ * You are given coins of different denominations and a total 
+ * amount of money amount. Write a function to compute the fewest number of 
+ * coins that you need to make up that amount. If that amount of money 
+ * cannot be made up by any combination of the coins, return -1.
+ * Example 1:
+ * 
+ * Input: coins = [1, 2, 5], amount = 11
+ * Output: 3 
+ * Explanation: 11 = 5 + 5 + 1 
+ * Example 2:
+ * 
+ * 
+ * Input: coins = [2], amount = 3
+ * Output: -1 
+ * Note:
+ * You may assume that you have an infinite number of each kind of coin.
  */
 package com.pk.algorithms.dynamicprogramming;
 
