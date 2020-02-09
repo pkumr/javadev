1edce9c0552d8cf2fde7b89e8fb34de1d6ba57cd
Parveen Kumar <parveen.kumar6@gmail.com>
2020-02-08 23:05:26 -0800

    MaximalRectangle

--- algorithms/dynamicprogramming/MaximalRectangle.java
+++ algorithms/dynamicprogramming/MaximalRectangle.java
@@ -1,7 +1,21 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Reference - Leetcode
+ * 85. Maximal Rectangle
+ * 
+ * Given a 2D binary matrix filled with 0's and 1's, 
+ * find the largest rectangle containing only 1's and return its area.
+ * 
+ * Example:
+ * 
+ * Input:
+ * [
+ *   ["1","0","1","0","0"],
+ *   ["1","0","1","1","1"],
+ *   ["1","1","1","1","1"],
+ *   ["1","0","0","1","0"]
+ * ]
+ *
+ * Output: 6
  */
 package com.pk.algorithms.dynamicprogramming;
 
