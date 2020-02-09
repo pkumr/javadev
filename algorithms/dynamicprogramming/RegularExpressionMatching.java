1b4555fd96dea4a1c55b3575df33946b1024480d
Parveen Kumar <parveen.kumar6@gmail.com>
2020-02-08 23:07:04 -0800

    Regular Expression

---
+++ algorithms/dynamicprogramming/RegularExpressionMatching.java
@@ -0,0 +1,34 @@
+/*
+ * Reference - Leetcode 10
+ * 10. Regular Expression Matching
+ * 
+ * Given an input string (s) and a pattern (p), implement regular expression 
+ * matching with support for '.' and '*'.
+ *
+ * '.' Matches any single character.
+ * '*' Matches zero or more of the preceding element.
+ * The matching should cover the entire input string (not partial).
+ * 
+ * Note:
+ * s could be empty and contains only lowercase letters a-z. 
+ * p could be empty and contains only lowercase letters a-z, 
+ * and characters like . or *.
+ * 
+ * Example 1:
+ * 
+ * Input:
+ * s = "aa"
+ * p = "a"
+ * Output: false
+ * Explanation: "a" does not match the entire string "aa".
+ * 
+ */
+package com.pk.algorithms.dynamicprogramming;
+
+/**
+ *
+ * @author parveenkumar
+ */
+public class RegularExpressionMatching {
+    
+}
