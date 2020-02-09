243d89b38ea5c8745d5ad139a339c717d7516d17
Parveen Kumar <parveen.kumar6@gmail.com>
2020-02-08 22:59:13 -0800

    EditDistance

--- algorithms/dynamicprogramming/EditDistance.java
+++ algorithms/dynamicprogramming/EditDistance.java
@@ -1,7 +1,24 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Reference - Leetcode
+ * 72. Edit Distance
+ * 
+ * Given two words word1 and word2, find the minimum number of 
+ * operations required to convert word1 to word2.
+ * 
+ * You have the following 3 operations permitted on a word:
+ * 
+ * Insert a character
+ * Delete a character
+ * Replace a character
+ * Example 1:
+ * 
+ * Input: word1 = "horse", word2 = "ros"
+ * Output: 3
+ * Explanation: 
+ * horse -> rorse (replace 'h' with 'r')
+ * rorse -> rose (remove 'r')
+ * rose -> ros (remove 'e')
+ * 
  */
 package com.pk.algorithms.dynamicprogramming;
 
