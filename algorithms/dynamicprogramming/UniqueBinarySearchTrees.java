3d4d1f1ac41273fea5f317358d0019af0c8720d9
Parveen Kumar <parveen.kumar6@gmail.com>
2020-02-08 23:08:21 -0800

    UniqueBinarySearchTrees

--- algorithms/dynamicprogramming/UniqueBinarySearchTrees.java
+++ algorithms/dynamicprogramming/UniqueBinarySearchTrees.java
@@ -1,7 +1,22 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Reference - Leetcode
+ * 96. Unique Binary Search Trees
+ * 
+ * Given n, how many structurally unique BST's 
+ * (binary search trees) that store values 1 ... n?
+ * 
+ * Example:
+ * 
+ * Input: 3
+ * Output: 5
+ * Explanation:
+ * Given n = 3, there are a total of 5 unique BST's:
+ *        1         3     3      2      1
+ *         \       /     /      / \      \
+ *          3     2     1      1   3      2
+ *         /     /       \                 \
+ *        2     1         2                 3
+ * 
  */
 package com.pk.algorithms.dynamicprogramming;
 
