ae25e88803de058f39bd21da8071a3fce4f9bf60
Parveen Kumar <parveen.kumar6@gmail.com>
2020-02-09 21:46:08 -0800

    using dp

--- algorithms/dynamicprogramming/RegularExpressionMatching.java
+++ algorithms/dynamicprogramming/RegularExpressionMatching.java
@@ -74,4 +74,48 @@ public class RegularExpressionMatching {
             return fistMatch && isMatchRecursion(text.substring(1), pattern.substring(1));
         }
     }
+    private boolean isMatchDP(String s, String p){
+        if (s == null || p == null) {
+            return false;
+        }
+        /*
+         * dp = {
+         * { true, false, false, false },
+         * { false, true, false, false },
+         * { false, false, true, true },
+         * { false, false, false, true}
+         * }
+         * */
+        // above is final dp with input in main
+        //  s = "abb";
+        //  p = ".b*";
+        // dp[0][0] is marked true;
+        //
+        boolean[][] dp = new boolean[s.length()+1][p.length()+1];
+        dp[0][0] = true;
+        for (int i = 0; i < p.length(); i++) {
+            if (p.charAt(i) == '*' && dp[0][i-1]) {
+                System.out.println("[0][" + (i + 1)+ "]");
+                dp[0][i+1] = true;
+            }
+        }
+        for (int i = 0 ; i < s.length(); i++) {
+            for (int j = 0; j < p.length(); j++) {
+                if (p.charAt(j) == '.') {
+                    dp[i+1][j+1] = dp[i][j];
+                }
+                if (p.charAt(j) == s.charAt(i)) {
+                    dp[i+1][j+1] = dp[i][j];
+                }
+                if (p.charAt(j) == '*') {
+                    if (p.charAt(j-1) != s.charAt(i) && p.charAt(j-1) != '.') {
+                        dp[i+1][j+1] = dp[i+1][j-1];
+                    } else {
+                        dp[i+1][j+1] = (dp[i+1][j] || dp[i][j+1] || dp[i+1][j-1]);
+                    }
+                }
+            }
+        }
+        return dp[s.length()][p.length()];
+    }
 }
