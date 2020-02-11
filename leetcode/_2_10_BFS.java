9a605ec44f45e7f770b16778e89a915f685750a9
Parveen Kumar <parveen.kumar6@gmail.com>
2020-02-10 23:35:20 -0800

    BFS

---
+++ leetcode/_2_10_BFS.java
@@ -0,0 +1,22 @@
+package com.breadthfirst;
+/*
+ * Date - 2/10/2020 - 2/14/2020
+ * 1. Number of Islands (LC# 200) M (Also DFS)
+ * 2. Remove Invalid Parentheses (LC# 301) H (Also DFS)
+ * 3. Perfect Squares (LC# 279) M
+ * 4. Symmetric Tree (LC# 101) E (Also DFS)
+ * 5. Binary Tree Right Side View (LC# 199) M
+ * 6. Word Ladder (LC# 127) M
+ * 7. Word Ladder II (LC# 126) H
+ * 8. Binary Tree Level Order Traversal (LC# 102) M
+ * 9. Binary Tree Level Order Traversal II (LC# 107) E
+ * 10. Binary Tree Zigzag Level Order Traversal (LC# 103) M
+ * 11. Clone Graph (LC# 133) M
+ * 12. Course Schedule (LC# 207) M
+ * 13. Course Schedule II (LC# 210) M
+ * 14. Cut Off Trees for Golf Event (LC# 675) H
+ *
+ * */
+
+public class _2_10_BFS {
+}
