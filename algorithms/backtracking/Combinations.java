2818f8a006c32302d57b034f1a6d01d30841b484
Parveen Kumar <parveen.kumar6@gmail.com>
2020-02-13 21:12:56 -0800

    clean up

--- algorithms/backtracking/Combinations.java
+++ algorithms/backtracking/Combinations.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 
  */
 package com.pk.algorithms.backtracking;
 
@@ -27,5 +25,5 @@ public class Combinations {
             backtrack(list,templist,n,k,i+1);
             templist.remove(templist.size()-1);
         }
-}
+    }
 }
