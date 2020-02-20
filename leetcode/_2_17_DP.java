9f50f091db82a0134981a2099403889805a4a641
Parveen Kumar <parveen.kumar6@gmail.com>
2020-02-19 22:32:46 -0800

    DP

--- leetcode/_2_17_DP.java
+++ leetcode/_2_17_DP.java
@@ -18,5 +18,84 @@ package com.dynamicprog;
 *
 * */
 public class _2_17_DP {
+    /*
+    * LC# 62 Medium
+    *
+    * A robot is located at the top-left corner of mxn grid.
+    *
+    * The robot can only move either down or right at any point in time. The robot is trying
+    * to reach the bottom-right corner of the grid.
+    * How many possible unique paths are there?
+    *
+    * Note: m and n will be at most 100.
+    *
+    * Example:
+    *   Input  : m = 3 and n = 2
+    *   Output : 3
+    *   Explanation :
+    *       From the top-left corner, there are a total of 3 ways to
+    *       reach the bottom-right corner:
+    *           1. Right -> Right -> Down
+    *           2. Right -> Down -> Right
+    *           3. Down -> Right -> Right
+    * */
+
+    /*
+    * Approach# 1
+    * Recursion
+    *   The idea is robot can move either right or down. right movements will be in same row
+    *   and down movements will be in same column.
+    *   if we have m - 3 (rows) and n - 2 columns
+    *
+    *   And if we have to reach in second row second column => cell(1,1)
+    *   cell(1,1) can be reached from row 0 and column 1 ie from cell(0, 1)
+    *   or from row 1 and column 0 ie from cell (1, 0)
+    *
+    *   So to generalize this, cell (m, n) can be reached from either cell(m-1, n) or cell(m, n-1)
+    *
+    *   Now to we use recursion to achieve this.
+    *
+    * */
+    private int uniquePathsRecursion(int m, int n){
+        //if we have only one row or only one column then there is only 1 way.
+        if(m == 1 || n == 1){
+            return 1;
+        }
+        return uniquePathsRecursion(m - 1, n) + uniquePathsRecursion(m, n-1);
+    }
+    /*
+    * Approach 2
+    * Dynamic Programming
+    *
+    * */
+
+    /*
+    * LC# 63 Medium
+    *
+    * A robot is located at the top-left corner of a mxn grid.
+    *
+    * The robot can only move either down or right at any point in time. The robot is trying
+    * to reach the bottom-right corner of the grid.
+    *
+    * Now consider if obstacles are added to the girds.
+    * How many unique paths would be there?
+    *
+    * Example:
+    *   Input :
+    *   [
+    *       [0, 0, 0],
+    *       [0, 1, 0],
+    *       [0, 0, 0]
+    *   ]
+    *   Output : 2
+    *
+    * Explanation:
+    *   There is one obstacle in the middle of 3x3 grid above.
+    *   There are two ways to reach the bottom-right corner.
+    *       1. Right -> Right -> Down -> Down
+    *       2. Down -> Down -> Right -> Right
+    *
+    *
+    * */
 
 }
