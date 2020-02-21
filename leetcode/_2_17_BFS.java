d74894c77a7e4951cba0ad3146f040727fa9d872
Parveen Kumar <parveen.kumar6@gmail.com>
2020-02-20 23:59:45 -0800

    BFS

--- leetcode/_2_17_BFS.java
+++ leetcode/_2_17_BFS.java
@@ -662,7 +662,7 @@ public class  _2_17_BFS {
         for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++)
                 if (pacific[i][j] && atlantic[i][j])
-                    res.add(Arrays.asList(new Integer[] {i, j}));
+                    res.add(Arrays.asList( i, j));
         return res;
     }
 
@@ -727,7 +727,7 @@ public class  _2_17_BFS {
             for(int k = 0; k < 4; k++){
                 int x = current[0] + shift[k];
                 int y = current[1] + shift[k + 1];
-                    if(x >= 0 && x < n && y >= 0 && y < n && !visited[x][y] && matrix[x][y] > matrix[current[0]][current[1]]){
+                    if(x >= 0 && x < n && y >= 0 && y < n && !visited[x][y] && matrix[x][y] >= matrix[current[0]][current[1]]){
                     visited[x][y] = true;
                     queue.offer(new int[]{x, y});
                 }
