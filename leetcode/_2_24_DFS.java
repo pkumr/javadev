4d1d9a1923f9aae4b4e20ed5f2a008ba3df767df
Parveen Kumar <parveen.kumar6@gmail.com>
2020-02-25 23:07:28 -0800

    DFS

--- leetcode/_2_24_DFS.java
+++ leetcode/_2_24_DFS.java
@@ -1,4 +1,11 @@
 package com.depthfirst;
+/*
+* Date - 2/24/2020 to 3/1/2020
+* 1. Nested List Weight Sum (LC# 339)
+* 2. Sum of Distances in Tree (LC# 834)
+*
+*
+* */
 
 public class _2_24_DFS {
 }
