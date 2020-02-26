72a2b02078e1392307b0aa84753bdc12d9ce4204
Parveen Kumar <parveen.kumar6@gmail.com>
2020-02-25 23:07:50 -0800

    BFS

--- leetcode/_2_24_BFS.java
+++ leetcode/_2_24_BFS.java
@@ -1,4 +1,15 @@
 package com.breadthfirst;
-
+/*
+* Date - 2/24/2020 to 3/1/2020
+* 1. Shortest Distance from All Buildings (LC# 317)
+* 2. Find Bottom Left Tree Value (LC# 513)
+* 3. Cat and Mouse (LC# 913)
+* 4. Walls and Gates (LC# 286)
+* 5. Find Largest Value in Each Tree Row (LC# 515)
+* 6. Minimum Height Trees (LC# 310)
+*
+*
+*
+* */
 public class _2_24_BFS {
 }
