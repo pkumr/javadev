b0d0bb4bcee5290bce73f3cf616b307834f3a326
Parveen Kumar <parveen.kumar6@gmail.com>
2020-02-26 23:12:57 -0800

    DFS

--- leetcode/_2_24_DFS.java
+++ leetcode/_2_24_DFS.java
@@ -1,8 +1,21 @@
 package com.depthfirst;
 /*
 * Date - 2/24/2020 to 3/1/2020
-* 1. Nested List Weight Sum (LC# 339)
-* 2. Sum of Distances in Tree (LC# 834)
+* 1. Sum of Distances in Tree (LC# 834)
+* 2. House Robber III (LC# 337)
+* 3. Nested List Weight Sum II (LC# 364)
+* 3. Remove Boxes (LC# 546)
+* 4. Longest Increasing Path in a Matrix (LC# 329)
+* 5. Cracking the Safe (LC# 753)
+* 6. Unique Paths III (LC# 980)
+* 7. Robot Room Cleaner (LC# 489)
+* 8. Most Stones Removed with Same Row or Column (LC# 947)
+* 9. Shopping Offer (LC# 638)
+* 10. Validate Binary Search Tree (LC# 98)
+* 11. Flatten a Multilevel Doubly Linked List (LC# 430)
+* 12. Populating Next Right Pointer in Each Node II (LC# 117)
+* 13. Target Sum (LC# 494)
+* 14. Delete Nodes and Return Forest (LC# 1110)
 *
 *
 * */
