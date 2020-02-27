ff1ffd76c576e57177b831218b86fb58f7f84187
Parveen Kumar <parveen.kumar6@gmail.com>
2020-02-26 23:12:28 -0800

    BFS

--- leetcode/_2_24_BFS.java
+++ leetcode/_2_24_BFS.java
@@ -7,6 +7,14 @@ package com.breadthfirst;
 * 4. Walls and Gates (LC# 286)
 * 5. Find Largest Value in Each Tree Row (LC# 515)
 * 6. Minimum Height Trees (LC# 310)
+* 7. Is Graph Bipartite (LC# 785)
+* 8. Maximum Depth of N-ary Tree (LC# 559)
+* 9. Cheapest Flights Within K Stops (LC# 787)
+* 10. Shortest Bridge (LC# 934)
+* 11. Cut Off Trees for Golf Event (LC# 675)
+* 12. Web Crawler (LC# 1236)
+* 13. Escape a Large Maze (LC# 1036)
+* 14. Bus Routes (LC# 815)
 *
 *
 *
