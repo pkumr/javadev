68e91081515887908b8f55d8526d0821e53c5e7f
Parveen Kumar <parveen.kumar6@gmail.com>
2020-03-01 20:19:27 -0800

    DFS

--- leetcode/_3_02_DFS.java
+++ leetcode/_3_02_DFS.java
@@ -2,19 +2,20 @@ package com.depthfirst;
 /*
 * Date :- 3/1/2020 to 3/8/2020
 *
-* 1110	Delete Nodes And Return Forest
-* 897	Increasing Order Search Tree
-* 872	Leaf-Similar Trees
-* 924	Minimize Malware Spread
-* 366	Find Leaves of Binary Tree
-* 664	Strange Printer
-* 749	Contain Virus
-* 721	Accounts Merge
-* 129	Sum Root to Leaf Numbers
-* 971	Flip Binary Tree To Match Preorder Traversal
-* 472	Concatenated Words
-* 968	Binary Tree Cameras
-*
+* 1. Delete Nodes And Return Forest (LC# 1110)
+* 2. Increasing Order Search Tree (LC# 897)
+* 3. Leaf-Similar Trees (LC# 872)
+* 4. Minimize Mal ware Spread (LC# 924)
+* 5. Find Leaves of Binary Tree (LC# 366)
+* 6. Strange Printer (LC# 664)
+* 7. Contain Virus (LC# 749)
+* 8. Accounts Merge (LC# 721)
+* 9. Sum Root to Leaf Numbers (LC# 129)
+* 10. Flip Binary Tree To Match PreOrder Traversal (LC# 971)
+* 11. Concatenated Words (LC# 472)
+* 12. Binary Tree Cameras (LC# 968)
+* 13. Number of Distinct Islands (LC# 694)
+* 14. Swim in Rising Water (LC# 778)
 *
 * */
 
