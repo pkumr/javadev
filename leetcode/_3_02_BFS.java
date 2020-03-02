90c002315fa96bf11f9364911a3170405563f117
Parveen Kumar <parveen.kumar6@gmail.com>
2020-03-01 20:14:07 -0800

    BFS

--- leetcode/_3_02_BFS.java
+++ leetcode/_3_02_BFS.java
@@ -1,16 +1,20 @@
 package com.breadthfirst;
 /*
 * Date :- 3/1/2020 to 3/8/2020
-*
-* 261	Graph Valid Tree
-* 854	K-Similar Strings
-* 429	N-ary Tree Level Order Traversal
-* 864	Shortest Path to Get All Keys
-* 323	Number of Connected Components in an Undirected Graph
-* 752	Open the Lock
-* 1091	Shortest Path in Binary Matrix
-* 1284	Minimum Number of Flips to Convert Binary Matrix to Zero Matrix
-*
+* 1. Graph Valid Tree (LC# 261)
+* 2. K-Similar Strings (LC# 854)
+* 3. N-ary Tree Level Order Traversal (LC# 429)
+* 4. Shortest Path to Get All Keys (LC# 864)
+* 5. Number of Connected Components in an Undirected Graph (LC# 323)
+* 6. Open the Lock (LC# 752)
+* 7. Shortest Path in Binary Matrix (LC# 1091)
+* 8. Minimum Number of Flips to Convert Binary Matrix to Zero Matrix (LC# 1284)
+* 9. Cousins in Binary Tree (LC# 993)
+* 10. Minimum Moves to Move a Box to Their Target Location (LC# 1263)
+* 11. Minimum Knight Moves (LC# 1197)
+* 12. Web Crawler Multithreaded (LC# 1242)
+* 13. Shortest Path with Alternating Colors (LC# 1129)
+* 14. Minimum Moves to Reach Target with Rotations (LC# 1210)
 *
 *
 * */
