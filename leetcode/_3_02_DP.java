82403d2d8033d480273b4d6a2879fe2d9b7fe04b
Parveen Kumar <parveen.kumar6@gmail.com>
2020-03-02 22:14:39 -0800

    DP

--- leetcode/_3_02_DP.java
+++ leetcode/_3_02_DP.java
@@ -2,12 +2,21 @@ package com.dynamicprog;
 /*
 *
 * Date - 2/24/2020 to 3/1/2020
-* 
-* 300	Longest Increasing Subsequence
-* 1140	Stone Game II
-* 746	Min Cost Climbing Stairs
-* 741	Cherry Pickup
-* 363	Max Sum of Rectangle No Larger Than K
+* 1. Longest Increasing Subsequence (LC# 300)
+* 2. Stone Game II (LC# 1140)
+* 3. Min Cost Climbing Stairs (LC# 746)
+* 4. Cherry Pickup (LC# 741)
+* 5. Max Sum of Rectangle No Larger Than K (LC# 363)
+* 6. Longest Palindromic Subsequence (LC# 516)
+* 7. Wildcard Matching (LC# 44)
+* 8. Russian Doll Envelopes (LC# 354)
+* 9. Dungeon Game (LC# 174)
+* 10. Stone Game (LC# 877)
+* 11. Maximum Length of Repeated SubArray (LC# 718)
+* 12. Partition to K Equal Sum Subsets (LC# 698)
+* 13. Count Different Palindromic Subsequences (LC# 730)
+* 14. Odd Even Jump (LC# 975)
+*
 * */
 public class _3_02_DP {
 }
