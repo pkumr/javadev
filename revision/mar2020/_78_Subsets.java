783338e04d15f2608c99fa8c2d76051b392157d1
Parveen Kumar <parveen.kumar6@gmail.com>
2020-03-09 22:38:53 -0700

    Subset - Desc

--- revision/mar2020/_78_Subsets.java
+++ revision/mar2020/_78_Subsets.java
@@ -1,4 +1,24 @@
 package com.revision.mar2020;
-
+/*
+* Given a set of distinct integers, nums, return all possible subsets (the power set).
+*
+* Note: The solution set must not contain duplicate subsets.
+*
+* Example:
+*
+* Input: nums = [1,2,3]
+* Output:
+* [
+*   [3],
+*   [1],
+*   [2],
+*   [1,2,3],
+*   [1,3],
+*   [2,3],
+*   [1,2],
+*   []
+* ]
+*
+* */
 public class _78_Subsets {
 }
--- revision/mar2020/_90_SubsetsII.java
+++ revision/mar2020/_90_SubsetsII.java
@@ -1,4 +1,23 @@
 package com.revision.mar2020;
-
+/*
+* Given a collection of integers that might contain duplicates,
+* nums, return all possible subsets (the power set).
+*
+* Note: The solution set must not contain duplicate subsets.
+*
+* Example:
+*
+* Input: [1,2,2]
+* Output:
+* [
+*   [2],
+*   [1],
+*   [1,2,2],
+*   [2,2],
+*   [1,2],
+*   []
+* ]
+*
+* */
 public class _90_SubsetsII {
 }
