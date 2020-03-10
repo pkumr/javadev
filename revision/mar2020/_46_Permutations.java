484c38b3695c0382f0e9c8a747c640d4dd69671e
Parveen Kumar <parveen.kumar6@gmail.com>
2020-03-09 22:38:12 -0700

    Permu - Desc

--- revision/mar2020/_46_Permutations.java
+++ revision/mar2020/_46_Permutations.java
@@ -1,4 +1,20 @@
 package com.revision.mar2020;
-
+/*
+* Given a collection of distinct integers, return all possible permutations.
+*
+* Example:
+*
+* Input: [1,2,3]
+* Output:
+* [
+*  [1,2,3],
+*  [1,3,2],
+*  [2,1,3],
+*  [2,3,1],
+*  [3,1,2],
+*  [3,2,1]
+* ]
+*
+* */
 public class _46_Permutations {
 }
--- revision/mar2020/_47_PermutationsII.java
+++ revision/mar2020/_47_PermutationsII.java
@@ -1,4 +1,19 @@
 package com.revision.mar2020;
+/*
+* Given a collection of numbers that might contain duplicates,
+* return all possible unique permutations.
+*
+* Example:
+*
+* Input: [1,1,2]
+* Output:
+* [
+*   [1,1,2],
+*   [1,2,1],
+*   [2,1,1]
+* ]
+*
+* */
 
 public class _47_PermutationsII {
 }
