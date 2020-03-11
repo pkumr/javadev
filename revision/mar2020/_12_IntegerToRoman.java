88811b1ddb0bec4b375b1607e001f8017ac80949
Parveen Kumar <parveen.kumar6@gmail.com>
2020-03-10 21:27:06 -0700

    Tues-L

---
+++ revision/mar2020/_12_IntegerToRoman.java
@@ -0,0 +1,19 @@
+package com.revision.mar2020;
+/*
+* Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+* Symbol       Value
+* I             1
+* V             5
+* X             10
+* L             50
+* C             100
+* D             500
+* M             1000
+* For example, two is written as II in Roman numeral, just two one's added together.
+* Twelve is written as, XII, which is simply X + II. The number twenty seven is
+* written as XXVII, which is XX + V + II.
+*
+*
+* */
+public class _12_IntegerToRoman {
+}
