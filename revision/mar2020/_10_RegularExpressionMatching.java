461874de70e59e1d4a275ee1ed81704b073a311d
Parveen Kumar <parveen.kumar6@gmail.com>
2020-03-10 21:26:23 -0700

    Tues-L

---
+++ revision/mar2020/_10_RegularExpressionMatching.java
@@ -0,0 +1,19 @@
+package com.revision.mar2020;
+/*
+* Given an input string (s) and a pattern (p), implement regular
+* expression matching with support for '.' and '*'.
+*
+* '.' Matches any single character.
+* '*' Matches zero or more of the preceding element.
+*
+* The matching should cover the entire input string (not partial).
+*
+* Note:
+*
+* s could be empty and contains only lowercase letters a-z.
+* p could be empty and contains only lowercase letters a-z, and characters like . or *.
+*
+*
+* */
+public class _10_RegularExpressionMatching {
+}
