c2f65899b2c2929c238e3975c34792fe004ed1f3
Parveen Kumar <parveen.kumar6@gmail.com>
2020-04-03 21:42:29 -0700

    new

--- revision/mar2020/_42_TrappingRainWater.java
+++ revision/mar2020/_42_TrappingRainWater.java
@@ -1,4 +1,15 @@
 package com.revision.mar2020;
-
+/*
+* Given n non-negative integers representing an elevation map where
+* the width of each bar is 1, compute how much water it is able to
+* trap after raining.
+*
+* Example:
+* Input: [0,1,0,2,1,0,1,3,2,1,2,1]
+* Output: 6
+*
+* Topics – Array, Two Pointer, Stack
+*
+* */
 public class _42_TrappingRainWater {
 }
