f4a7193cdd767355f11b0a355a8eca2606c81ecc
Parveen Kumar <parveen.kumar6@gmail.com>
2020-04-05 19:02:24 -0700

    wkn

--- revision/mar2020/_106_ConstructBTInorderPostorderTraversal.java
+++ revision/mar2020/_106_ConstructBTInorderPostorderTraversal.java
@@ -1,4 +1,20 @@
 package com.revision.mar2020;
-
+/*
+* Given inorder and postorder traversal of a tree, construct the binary tree.
+* Note:
+* You may assume that duplicates do not exist in the tree.
+*
+* For example, given
+* inorder = [9,3,15,20,7]
+* postorder = [9,15,7,20,3]
+* Return the following binary tree:
+*       3
+*      / \
+*     9 20
+*       / \
+*      15 7
+*
+* Topics – Array, Tree, Depth-First-Search
+* */
 public class _106_ConstructBTInorderPostorderTraversal {
 }
--- revision/mar2020/_111_MinimumDepthBinaryTree.java
+++ revision/mar2020/_111_MinimumDepthBinaryTree.java
@@ -1,4 +1,21 @@
 package com.revision.mar2020;
-
+/*
+* Given a binary tree, find its minimum depth. The minimum depth is the
+* number of nodes along the shortest path from the root node down to the
+* nearest leaf node.
+*
+* Note: A leaf is a node with no children.
+*
+* Example:
+* Given binary tree [3,9,20,null,null,15,7],
+*       3
+*      / \
+*     9 20
+*       / \
+*      15 7
+* return its minimum depth = 2.
+*
+* Topics – Array, Tree, Depth-First-Search, Breadth-First Search
+* */
 public class _111_MinimumDepthBinaryTree {
 }
