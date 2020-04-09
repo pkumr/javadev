263381f4a6a7b0e96ffabe56c829e608db97fd91
Parveen Kumar <parveen.kumar6@gmail.com>
2020-04-08 22:15:10 -0700

    backlog

--- revision/mar2020/_301_RemoveInvalidParentheses.java
+++ revision/mar2020/_301_RemoveInvalidParentheses.java
@@ -1,4 +1,22 @@
 package com.revision.mar2020;
-
+/*
+* Remove the minimum number of invalid parentheses in order to make the
+* input string valid. Return all possible results.
+* Note: The input string may contain letters other than the parentheses ( and ).
+*
+* Example 1:
+* Input: "()())()"
+* Output: ["()()()", "(())()"]
+*
+* Example 2:
+* Input: "(a)())()"
+* Output: ["(a)()()", "(a())()"]
+*
+* Example 3:
+* Input: ")("
+* Output: [""]
+*
+* Topics – BFS, DFS
+* */
 public class _301_RemoveInvalidParentheses {
 }
--- revision/mar2020/_340_LongestSubstringAtMostKDistChar.java
+++ revision/mar2020/_340_LongestSubstringAtMostKDistChar.java
@@ -1,4 +1,19 @@
 package com.revision.mar2020;
-
+/*
+* Given a string, find the length of the longest substring T that
+* contains at most k distinct characters.
+*
+* Example 1:
+* Input: s = "eceba", k = 2
+* Output: 3
+* Explanation: T is "ece" which its length is 3.
+*
+* Example 2:
+* Input: s = "aa", k = 1
+* Output: 2
+* Explanation: T is "aa" which its length is 2.
+*
+* Topics – Hash Table, String, Sliding Window
+* */
 public class _340_LongestSubstringAtMostKDistChar {
 }
