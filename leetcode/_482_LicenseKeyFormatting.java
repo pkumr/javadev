b3f89bcb7a6cebc0a304bc458ffd96a7b521e22e
Parveen Kumar <parveen.kumar6@gmail.com>
2020-04-18 21:06:30 -0700

    IVP

---
+++ leetcode/_482_LicenseKeyFormatting.java
@@ -0,0 +1,18 @@
+package com.interview.goog.process;
+
+public class _482_LicenseKeyFormatting {
+    public static void main(String[] args){
+        String S = "5F3Z-2e-9-w";
+        int K = 4;
+        System.out.println(new _482_LicenseKeyFormatting().licenseKeyFormatting(S, K));
+    }
+    public String licenseKeyFormatting(String S, int K) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = S.length() - 1; i >= 0; i--) {
+            if (S.charAt(i) != '-') {
+                sb.append(sb.length() % (K + 1) == K ? '-' : "").append(S.charAt(i));
+            }
+        }
+        return sb.reverse().toString().toUpperCase();
+    }
+}
