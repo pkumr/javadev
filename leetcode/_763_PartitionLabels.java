f52b511e5d778ef294ebb771cfaeadc8038576be
Parveen Kumar <parveen.kumar6@gmail.com>
2020-04-19 21:55:12 -0700

    IVP

---
+++ leetcode/_763_PartitionLabels.java
@@ -0,0 +1,28 @@
+package com.problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _763_PartitionLabels {
+    public static void main(String[] args){
+        String S = "ababcbacadefegdehijhklij";
+        System.out.println("Output :- " + new _763_PartitionLabels().partitionLabels(S));
+    }
+    //Greedy Approach
+    public List<Integer> partitionLabels(String S) {
+        int[] last = new int[26];
+        for(int i = 0; i < S.length(); i++){
+            last[S.charAt(i) - 'a'] = i;
+        }
+        int j = 0, a = 0;
+        List<Integer> answer = new ArrayList<>();
+        for(int i = 0; i < S.length(); i++){
+            j = Math.max(j, last[S.charAt(i) - 'a']);
+            if(i == j){
+                answer.add(i - a + 1);
+                a = i + 1;
+            }
+        }
+        return answer;
+    }
+}
