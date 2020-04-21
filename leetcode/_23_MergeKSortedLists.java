e653a18e2b719dbe871533fcf50b8c3bda32c659
Parveen Kumar <parveen.kumar6@gmail.com>
2020-04-20 22:54:48 -0700

    IVP

---
+++ leetcode/_23_MergeKSortedLists.java
@@ -0,0 +1,67 @@
+package com.problems;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class _23_MergeKSortedLists {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+
+    }
+    public static void main(String[] args){
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(5);
+
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+
+        ListNode l3 = new ListNode(2);
+        l3.next = new ListNode(6);
+
+        ListNode[] input = {l1, l2, l3};
+
+        _23_MergeKSortedLists.ListNode output = new _23_MergeKSortedLists().mergeKLists(input);
+        while (output != null){
+            System.out.print(output.val + "-->");
+            output = output.next;
+        }
+    }
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists==null||lists.length==0)
+            return null;
+        PriorityQueue<ListNode> queue= new PriorityQueue<ListNode>(lists.length,new Comparator<ListNode>(){
+            @Override
+            public int compare(ListNode o1,ListNode o2){
+                if (o1.val<o2.val)
+                    return -1;
+                else if (o1.val==o2.val)
+                    return 0;
+                else
+                    return 1;
+            }
+        });
+
+        ListNode dummy = new ListNode(0);
+        ListNode tail=dummy;
+
+        for (ListNode node:lists)
+            if (node!=null)
+                queue.add(node);
+
+        while (!queue.isEmpty()){
+            tail.next=queue.poll();
+            tail=tail.next;
+
+            if (tail.next!=null)
+                queue.add(tail.next);
+        }
+        return dummy.next;
+    }
+}
