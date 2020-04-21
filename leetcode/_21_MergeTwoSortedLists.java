317d567d74dff88c02619c9bf4953e6dfea4cebf
Parveen Kumar <parveen.kumar6@gmail.com>
2020-04-20 22:30:01 -0700

    IVP

---
+++ leetcode/_21_MergeTwoSortedLists.java
@@ -0,0 +1,40 @@
+package com.problems;
+
+public class _21_MergeTwoSortedLists {
+    public static void main(String[] args){
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+
+        ListNode output = new _21_MergeTwoSortedLists().mergeTwoLists(l1, l2);
+        while (output != null){
+            System.out.print(output.val + "-->");
+            output = output.next;
+        }
+    }
+    public static class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+    }
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        else if (l2 == null) {
+            return l1;
+        }
+        else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }
+        else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+}
