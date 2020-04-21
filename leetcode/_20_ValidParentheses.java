535d94cf3b714e1fbcd311da5bf95060aae7d300
Parveen Kumar <parveen.kumar6@gmail.com>
2020-04-20 21:38:53 -0700

    IVP

---
+++ leetcode/_20_ValidParentheses.java
@@ -0,0 +1,52 @@
+package com.problems;
+import java.util.HashMap;
+import java.util.Stack;
+public class _20_ValidParentheses {
+    // Hash table that takes care of the mappings.
+    private HashMap<Character, Character> mappings;
+    public _20_ValidParentheses(){
+        this.mappings = new HashMap<Character, Character>();
+        this.mappings.put(')', '(');
+        this.mappings.put('}', '{');
+        this.mappings.put(']', '[');
+    }
+    public static void main(String[] args){
+        String input = "()";
+        System.out.println("OutputOne:- " + new _20_ValidParentheses().isValid(input));
+        input = "()[]{}";
+        System.out.println("OutputTwo:- " + new _20_ValidParentheses().isValid(input));
+        input = "(]";
+        System.out.println("OutputThree:- " + new _20_ValidParentheses().isValid(input));
+        input = "([)]";
+        System.out.println("OutputFour:- " + new _20_ValidParentheses().isValid(input));
+        input = "{[]}";
+        System.out.println("OutputFive:- " + new _20_ValidParentheses().isValid(input));
+    }
+
+    public boolean isValid(String s) {
+        // Initialize a stack to be used in the algorithm.
+        Stack<Character> stack = new Stack<Character>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            // If the current character is a closing bracket.
+            if (this.mappings.containsKey(c)) {
+
+                // Get the top element of the stack. If the stack is empty, set a dummy value of '#'
+                char topElement = stack.empty() ? '#' : stack.pop();
+
+                // If the mapping for this bracket doesn't match the stack's top element, return false.
+                if (topElement != this.mappings.get(c)) {
+                    return false;
+                }
+            } else {
+                // If it was an opening bracket, push to the stack.
+                stack.push(c);
+            }
+        }
+
+        // If the stack still contains elements, then it is an invalid expression.
+        return stack.isEmpty();
+    }
+}
