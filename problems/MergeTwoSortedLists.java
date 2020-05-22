ce05072a4878bdf0d9022a145d77a6b82d99c522
Parveen Kumar <parveen.kumar6@gmail.com>
2020-05-22 16:21:28 -0700

    correction

--- problems/MergeTwoSortedLists.java
+++ problems/MergeTwoSortedLists.java
@@ -1,6 +1,6 @@
 package com.problems;
 
-public class _21_MergeTwoSortedLists {
+public class MergeTwoSortedLists {
     public static void main(String[] args){
         ListNode l1 = new ListNode(1);
         l1.next = new ListNode(2);
