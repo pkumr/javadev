9b3e3ea9310eff6f1ea732e0530a0396608ee1b7
Parveen Kumar <parveen.kumar6@gmail.com>
2020-06-17 20:55:49 -0700

    three

--- problems/3Sum.java
+++ problems/3Sum.java
@@ -13,7 +13,9 @@ public class 3Sum {
     public List<List<Integer>> threeSumTP(int[] nums){
         List<List<Integer> result = new ArrayList<>();
         Arrays.sort(nums);
-
+        for(int i = 0; i < nums.length;  i++){
+            if(i == 0 || nums[i - 1] != nums[i])
+                twoSumII(nums, i, result);
         return result;
     }
 
