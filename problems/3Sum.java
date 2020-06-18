1bdbe8bc7d2fb8d976a8d253f729abce10d69941
Parveen Kumar <parveen.kumar6@gmail.com>
2020-06-17 21:06:13 -0700

    three

--- problems/3Sum.java
+++ problems/3Sum.java
@@ -18,7 +18,20 @@ public class 3Sum {
                 twoSumII(nums, i, result);
         return result;
     }
-
+    private void twoSumII(int[] nums, int i, List<List<Integer>> result){
+        int low = i + 1;
+        int high = nums.length - 1;
+        while(low < high) {
+            int sum = nums[i] + nums[low] + nums[high];
+            if(sum < 0 || (low > i + 1 && nums[low] == nums[low - 1])){
+                low++;
+            } else if (sum > 0 || (high < nums.length - 1 && nums[high] == nums[high + 1])){
+                high--;
+            }else{
+                result.add(Arrays.asList(nums[i], nums[low++], nums[high--]));
+            }
+        }
+    }
 
 
     public List<List<Integer>> threeSum(int[] nums) {
