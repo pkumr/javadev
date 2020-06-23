c207556003d1492c728414bde3d8033a98506772
Parveen Kumar <parveen.kumar6@gmail.com>
2020-06-22 21:44:56 -0700

    CPTemplate

---
+++ compjava/Model.java
@@ -0,0 +1,57 @@
+package com.codeforces;
+/*
+* Quick Template
+* @author : Parveen Kumar (fin_un)
+*
+*
+* */
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+
+
+public class Model {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+
+        Model p = new Model();
+        p.solve(in, out);
+        out.close();
+    }
+
+    public void solve(InputReader in, PrintWriter out) {
+
+    }
+}
+class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+        reader = new BufferedReader(new InputStreamReader(stream));
+        tokenizer = null;
+    }
+
+    public String next() {
+        while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+            try {
+                tokenizer = new StringTokenizer(reader.readLine());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return tokenizer.nextToken();
+    }
+
+    public int nextInt() {
+        return Integer.parseInt(next());
+    }
+}
