af60bb86cc3dc6a39bd0dc80810f95365b9e1576
Parveen Kumar <parveen.kumar6@gmail.com>
2020-06-28 14:26:05 -0700

    updated oops file

--- compjava/JBasicOOPs.java
+++ compjava/JBasicOOPs.java
@@ -13,6 +13,18 @@ public class JBasicOOPs {
  * properties. then class Person HAS A relation with name and
  * age
  *
+ *
+ * Example: When to go for IS-A and HAS-A relation
+ *  1.  when we have Parent class and we need whole functionality of Parent
+ *      class we extend Parent class in Child class. This way all methods and
+ *      functionality of Parent is available in Child class. So we can say
+ *      IS-A relation when we need total functionality of Parent in Child.
+ *      (Whole Functionality)
+ *
+ * 2.   when we have class A and we create instance of class A in class B and
+ *      use only one or two methods (not all available methods in A) of class A
+ *      in B class. This is where we went for HAS-A relation (PART of Functionality)
+ *
  * */
 class Person{
     String name;
@@ -68,3 +80,56 @@ class University{
 class Professor{
 
 }
+
+/*
+*                      Difference between Overloading & Overriding
+*   ----------------------------------------------------------------------------------------------------
+*   #      PROPERTY                OVERLOADING                 OVERRIDING
+*   1     method names            must be same                 must be same
+*   2     argument types         must be different            must be same
+*   3     method signature      must be different             must be same
+*   4     return type           no restriction                must be same (until 1.4 V) co-variant (1.5)
+*   5     private, static       can be overloaded             can't be overridden
+*         final methods
+*   6     Access Modifiers      no restrictions               the scope of access modifier
+*                                                             can't be reduced but can be increased
+*   7     throws clause         no restrictions               if child class method throws any
+*                                                             checked exception, then parent class
+*                                                             method must throw the same checked
+*                                                             exception or its parent. No restriction
+*                                                             for unchecked exception
+*   8    method resolution      taken care by compiler        taken care by JVM based on runtime
+*                               based on reference type       object
+*   9    Also known as          Compile Time Polymorphism     Runtime Polymorphism
+*                               Static Polymorphism           Dynamic Polymorphism
+*                               Early Binding                 Late Binding
+*
+* */
+
+/*
+*
+* *******************  PARENT / CHILD Reference *********************
+*
+*   If we don't know exact runtime type of object then we should go for
+*   parent reference.
+*   Example :
+*       Let's have an arrayList hold objects of person, customer, string
+*       and string buffer.
+*       When we are trying to access objects in arraylist then,
+*       First element present in arraylist can be any type. It may be
+*       customer object, person object, string object or string buffer
+*       object.
+*       Hence return type of get Method is object type, that can hold
+*       any object
+*
+*   ChildClass child = new ChildClass()     |      ParentClass parent = new ChildClass();
+*   eg ArrayList al = new ArrayList();      |      List l = new ArrayList();
+*                                           |
+*   1. Runtime type of object is known      | 1.   Exact Runtime type of object is not known
+*   2. we can use both child class and      | 2.   We can use only parent class methods
+*       parent class methods                |
+*   3. Here we can hold child object of     | 3.   Here we can hold child object of any type
+*       particular type (ArrayList Only)    |      in parent object (ex List can hold ArrayList
+*                                           |       LinkedList etc)
+*   
+* */
