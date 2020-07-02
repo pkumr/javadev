b24ec309e52841f3bae3349604ab351646874377
Parveen Kumar <parveen.kumar6@gmail.com>
2020-07-01 18:55:44 -0700

    exception

---
+++ compjava/JExceptions.java
@@ -0,0 +1,53 @@
+package com.compjava;
+/*
+* @author : Parveen Kumar
+*
+*   1.  Introduction
+*   2.  Runtime stack mechanism
+*   3.  Default exception handling in java
+*   4.  Exception Hierarchy
+*   5.  Customized exception handling by using try catch
+*   6.  Control flow in try catch
+*   7.  Methods to print exception information
+*   8.  try with multiple catch blocks
+*   9.  finally block
+*   10. difference between final, finally, finalize
+*   11. control flow in try-catch-finally
+*   12. control flow in nested try-catch-finally
+*   13. various possible combinations of try catch finally
+*   14. throw keyword
+*   15. throws keyword
+*   16. Exception handling keywords summary
+*   17. Various possible compile time errors in exception handling
+*   18. Customized or user defined exceptions
+*   19. Top -10 exceptions
+*   20. 1.7 version enhancements
+*           1.  try with resources.
+*           2.  multi-catch block
+*
+* */
+
+/*
+*
+* Exception - An unwanted event that disrupts the normal flow of program is Exceptions.
+* Types of Exceptions in JAVA :
+*       1.  Checked -- compile time
+*       2.  Unchecked -- usually runtime
+*       3.  Partially check (Throwable & Exception)
+*       4.  Fully checked
+*
+* Behavior of Following Exception
+*   IOException                     Checked --> Fully Checked
+*   RuntimeException                Unchecked
+*   InterruptedException            Checked --> Fully Checked
+*   Error                           Unchecked
+*   Throwable                       Checked (Partially Checked)
+*   ArithmeticException             Unchecked
+*   NullPointerException            Unchecked
+*   Exception                       Checked (Partially Checked)
+*   FileNotFoundException           Checked --> Fully Checked
+*
+*
+* */
+public class JExceptions {
+}
