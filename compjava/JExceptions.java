00943a0e7906b7d781d04a8d3733876d6d2e2e75
Parveen Kumar <parveen.kumar6@gmail.com>
2020-07-08 23:49:50 -0700

    exceptions

--- compjava/JExceptions.java
+++ compjava/JExceptions.java
@@ -1,31 +1,32 @@
 package com.compjava;
 
-/*
-* @author : Parveen Kumar
-*
-*   1.  Introduction
-*   2.  Runtime stack mechanism
-*   3.  Default exception handling in java
-*   4.  Exception Hierarchy
-*   5.  Customized exception handling by using try catch
-*   6.  Control flow in try catch
-*   7.  Methods to print exception information
-*   8.  try with multiple catch blocks
-*   9.  finally block
-*   10. difference between final, finally, finalize
-*   11. control flow in try-catch-finally
-*   12. control flow in nested try-catch-finally
-*   13. various possible combinations of try catch finally
-*   14. throw keyword
-*   15. throws keyword
-*   16. Exception handling keywords summary
-*   17. Various possible compile time errors in exception handling
-*   18. Customized or user defined exceptions
-*   19. Top -10 exceptions
-*   20. 1.7 version enhancements
-*           1.  try with resources.
-*           2.  multi-catch block
-*
+/**
+ *
+ * @author : Parveen Kumar
+ *
+ *   1.  Introduction
+ *   2.  Runtime stack mechanism
+ *   3.  Default exception handling in java
+ *   4.  Exception Hierarchy
+ *   5.  Customized exception handling by using try catch
+ *   6.  Control flow in try catch
+ *   7.  Methods to print exception information
+ *   8.  try with multiple catch blocks
+ *   9.  finally block
+ *   10. difference between final, finally, finalize
+ *   11. control flow in try-catch-finally
+ *   12. control flow in nested try-catch-finally
+ *   13. various possible combinations of try catch finally
+ *   14. throw keyword
+ *   15. throws keyword
+ *   16. Exception handling keywords summary
+ *   17. Various possible compile time errors in exception handling
+ *   18. Customized or user defined exceptions
+ *   19. Top -10 exceptions
+ *   20. 1.7 version enhancements
+ *           1.  try with resources.
+ *           2.  multi-catch block
+ *
 * */
 
 import java.io.BufferedReader;
